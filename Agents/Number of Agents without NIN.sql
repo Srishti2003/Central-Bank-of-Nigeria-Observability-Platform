fa866f23956d4c43cb192cd20bc12de32541bd33
Srishti Gautam <99689588+Srishti2003@users.noreply.github.com>
2025-11-06 12:34:18 +0530

    Create Number of Agents without NIN.sql

---
+++ Agents/Number of Agents without NIN.sql
@@ -0,0 +1,20 @@
+SELECT 
+    `publisher` AS `publisher_52aded`, 
+    count(DISTINCT `agent_code`) AS `COUNT_DISTINCT(agent_code)_324dd3`
+FROM (
+    WITH agents AS (
+        SELECT * 
+        FROM cbn_data.agent_bank_details_dist
+        UNION ALL
+        SELECT * 
+        FROM cbn_data.agent_bank_details_hashed_dist
+    )
+    SELECT * 
+    FROM agents
+) AS `virtual_table`
+WHERE 
+    `agent_NIN` IS NULL
+GROUP BY 
+    `publisher`
+ORDER BY 
+    `COUNT_DISTINCT(agent_code)_324dd3` DESC;
