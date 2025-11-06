92c44b19c4ac2b2b1930eb65b445c47de8ab12d2
Srishti Gautam <99689588+Srishti2003@users.noreply.github.com>
2025-11-06 12:25:52 +0530

    Create Number of Merchants without BVN.sql

---
+++ Merchants/Number of Merchants without BVN.sql
@@ -0,0 +1,10 @@
+SELECT 
+    `publisher` AS `publisher_52aded`, 
+    count(DISTINCT `merchant_code`) AS `COUNT_DISTINCT(merchant_code)_cc604b`
+FROM (
+    SELECT * 
+    FROM cbn_data.merchant_details_dist
+) AS `virtual_table`
+WHERE `merchant_bvn` IS NULL 
+GROUP BY `publisher`
+ORDER BY `COUNT_DISTINCT(merchant_code)_cc604b` DESC;
