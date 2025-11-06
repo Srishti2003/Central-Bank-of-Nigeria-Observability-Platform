31dc811a5928cb4c3dfc5ad36a5c105512f4508f
Srishti Gautam <99689588+Srishti2003@users.noreply.github.com>
2025-11-06 12:10:44 +0530

    Create Overutilized ATMs over time.sql

---
+++ ATM/Overutilized ATMs over time.sql
@@ -0,0 +1,17 @@
+SELECT 
+    toStartOfHour(toDateTime(`timestamp`)) AS `timestamp_d7e6d5`, 
+    `publisher` AS `publisher_52aded`, 
+    count(DISTINCT `atm_id`) AS `COUNT_DISTINCT(atm_id)_49ce20`
+FROM (
+    SELECT 
+        toDateTime(date_and_time) AS timestamp, 
+        cash_dispensed / cash_replenishment AS cash_ratio, 
+        *
+    FROM cbn_data.atm_cash_management_dist
+) AS `virtual_table`
+WHERE `cash_ratio` > 0.4
+GROUP BY 
+    toStartOfHour(toDateTime(`timestamp`)), 
+    `publisher`
+ORDER BY 
+    `COUNT_DISTINCT(atm_id)_49ce20` DESC;
